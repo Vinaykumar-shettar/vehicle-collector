5697c366c967202146ae46713f733d7efaaa6ac1
Vinaykumar-shettar <68851137+Vinaykumar-shettar@users.noreply.github.com>
2020-08-04 16:41:16 +0530

    Update vehicle-collector-test.cpp

--- vehicle-collector-test.cpp
+++ vehicle-collector-test.cpp
@@ -1,10 +1,22 @@
 #include "vehicle_collector.h"
 #include <gtest/gtest.h>
 
-TEST(COLLECT, one_telematics_is_converted_into_one_inventory) {
+TEST(COLLECT, motor_temp_telematics_is_converted_into_one_inventory) {
     Telematics t = {10, MOTOR_TEMP, 30.5};
     Vehicle v = telemetrics_to_inventory(t);
     ASSERT_EQ(v.motor_temp, 30.5);
+}
+
+TEST(COLLECT, battery_soc__telematics_is_converted_into_one_inventory) {
+    Telematics t = {10, BATTERY_SOC, 50.5};
+    Vehicle v = telemetrics_to_inventory(t);
+    ASSERT_EQ(v.battery_pc, 50.5);
+}
+
+TEST(COLLECT, battery_temp_telematics_is_converted_into_one_inventory) {
+    Telematics t = {10, BATTERY_TEMP, 25.5};
+    Vehicle v = telemetrics_to_inventory(t);
+    ASSERT_EQ(v.battery_temp, 25.5);
 }
 
 int main(int argc, char **argv) {
